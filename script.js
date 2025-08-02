46cdddf910b0d137e102097cb6b28785df1d9ff8
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2025-08-02 22:52:16 +0600

    started

---
+++ script.js
@@ -0,0 +1,22 @@
+ var typed = new Typed('#element', {
+      strings: ['Web Developer'],
+      typeSpeed: 50,
+    });
+
+    window.addEventListener('scroll',()=>{
+    const navbar = document.querySelector('nav');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+
+  // const hamburger = document.querySelector('.hamburger');
+  // const navLinks = document.querySelector('nav ul');
+
+  // hamburger.addEventListener('click', () => {
+  //   navLinks.classList.toggle('show');
+  // });
+
