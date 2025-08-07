9e5e72a4bc2bc5a900cc4a5cce9b6782103f9e71
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2025-08-08 02:24:50 +0600

    ready to deploy

--- script.js
+++ script.js
@@ -9,17 +9,17 @@ const navbar = document.querySelector('nav');
 window.addEventListener('scroll', () => {
   if (window.innerWidth <= 850) {
     if (window.scrollY < lastScrollY) {
-      // Scrolling up
+      
       navbar.classList.add('navbar-fixed');
       navbar.style.top = '0';
     } else {
-      // Scrolling down
-      navbar.style.top = '-100px'; // Hide navbar
+      
+      navbar.style.top = '-100px'; 
       setTimeout(() => navbar.classList.remove('navbar-fixed'), 300);
     }
     lastScrollY = window.scrollY;
   } else {
-    // Desktop: transparent when scrolling down, solid otherwise
+    
     if (window.scrollY > 0) {
       navbar.classList.add('scrolled');
     } else {
