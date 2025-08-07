38dd4b78243d501553548c49001ebea3ac900755
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2025-08-08 00:20:30 +0600

    ready for deploy

--- script.js
+++ script.js
@@ -18,7 +18,7 @@
 const textarea = document.querySelector('.messageinput');
 
 textarea.addEventListener('input', () => {
-  textarea.style.height = 'auto'; // reset
+  textarea.style.height = 'auto'; 
   textarea.style.height = textarea.scrollHeight + 'px';
 });
 const hamburger = document.querySelector('.hamburger');
@@ -26,20 +26,20 @@ const navLinks = document.querySelector('.nav-links');
 
 hamburger.addEventListener('click', function(e) {
   navLinks.classList.toggle('active');
-  e.stopPropagation(); // Prevent this click from bubbling up to document
+  e.stopPropagation(); 
 });
 
-// Hide nav-links when clicking outside
+
 document.addEventListener('click', function(e) {
   if (navLinks.classList.contains('active')) {
-    // If click is outside nav-links and hamburger
+    
     if (!navLinks.contains(e.target) && !hamburger.contains(e.target)) {
       navLinks.classList.remove('active');
     }
   }
 });
 
-// Optional: Hide menu when a nav link is clicked
+
 navLinks.addEventListener('click', function(e) {
   if (e.target.tagName === 'A') {
     navLinks.classList.remove('active');
